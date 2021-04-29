48957852f23eede4543391fffa61f7eb4d579426
SoheeKim <kimsh@eseict.com>
2021-04-29 10:10:08 +0900

    바닐라js에서 리덕스 사용

--- src/index.js
+++ src/index.js
@@ -1,45 +1,63 @@
-import {createStore} from "redux";
-//리덕스에서 createStore를 가져온다. 
-// store란? - state를 넣는 곳 즉 내 app에서 바뀌는 데이터가 들어갈 곳이다.
-// createStore? - 내 data를 넣을 곳을 만들어준다
-const add= document.getElementById("add");
-const minus= document.getElementById("minus");
-const number= document.querySelector("span");
-
-number.innerText=0;
-
-// reducer는 내 데이터를 수정하는 함수다
-const countModifier = (count=0,action) =>{
-    switch(action.type){
-        case "ADD":
-            return count+1;
-        case "MINUS":
-            return count-1;
-        default: 
-            return count;
-    }   
-
-}; 
-
-const countStore=createStore(countModifier);
-
-const onChange=()=>{
-   number.innerText=countStore.getState();
-}
-
-countStore.subscribe(onChange);
-
-const handleAdd=()=>{
-    countStore.dispatch({ type:"ADD" });
-}
-
-const handleMinus=()=>{
-    countStore.dispatch({ type:"MINUS" });
-}
-
-add.addEventListener("click",handleAdd);
-minus.addEventListener("click",handleMinus);
-
-
-//dispatch로 countModifier에 type이라는 메시지를 보낸다.
-//countStore.dispatch({type:"ADD"});
+import { createStore } from "redux";
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const ul = document.querySelector("ul");
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+const addToDo = text => {
+  return {
+    type: ADD_TODO,
+    text
+  };
+};
+const deleteToDo = id => {
+  return {
+    type: DELETE_TODO,
+    id
+  };
+};
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case ADD_TODO:
+      return [{ text: action.text, id: Date.now() }, ...state];
+      const newToDoObj = { text: action.text, id: Date.now() };
+      return [newToDoObj, ...state];
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== action.id);
+      const cleaned = state.filter(toDo => toDo.id !== action.id);
+      return cleaned;
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+store.subscribe(() => console.log(store.getState()));
+const dispatchAddToDo = text => {
+  store.dispatch(addToDo(text));
+};
+const dispatchDeleteToDo = e => {
+  const id = parseInt(e.target.parentNode.id);
+  store.dispatch(deleteToDo(id));
+};
+const paintToDos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(toDo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo);
+    li.id = toDo.id;
+    li.innerText = toDo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
+};
+store.subscribe(paintToDos);
+const onSubmit = e => {
+  e.preventDefault();
+  const toDo = input.value;
+  input.value = "";
+  dispatchAddToDo(toDo);
+};
+form.addEventListener("submit", onSubmit);
